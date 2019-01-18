719150879e114c0c20753a0efe37b78f57da5719
Jackson Wang <jw@Jacksons-iMac.attlocal.net>
2019-01-17 23:31:54 -0800

    updated removenode.js

--- Tree Data Structure/RemoveNode.js
+++ Tree Data Structure/RemoveNode.js
@@ -67,10 +67,26 @@ BinarySearchTree.prototype.push = function(key){
 
 BinarySearchTree.prototype.removeNode = function(node, key){
     let self = this;
+    // If the node doesn't exist, we simply return null.
     if (!node){
         return null;
     }
-    
+    /** 
+    * In the if...if else...else statement below, we check for the direction to travel
+    * If the given key is smaller than the current node's key, we go to left
+    * If the given key is greater than the current node's key, we go to right
+    * If the given key is equal to the current node's key, we know we found the to-be-removed node
+    * We use recursion to traverse the tree, every function call will return a node.
+    * We need to assign that node to its new parent. 
+    * For example, if we are removing a node with two children, we need to travel back eventually 
+    * and return that node reference to its parent and re-create the pointer.
+    * This is why we have something like node.left = self.removeNode(node.left, key); 
+    * The function call will return a node. Once we get that node, we assign node.left = that node.
+    * When we do that, on the call back to the parent call, we still need to remember to return
+    * the current node! That's why we have return node after the node.left = ... call.
+    * For every node, when the recursive call goes back, we need to return its current node to the
+    * parent.
+    **/
     if (key < node.key){
         node.left = self.removeNode(node.left, key);
         return node;
@@ -78,16 +94,42 @@ BinarySearchTree.prototype.removeNode = function(node, key){
         node.right = self.removeNode(node.right, key);
         return node;
     }else {
+        /** 
+        * The first statement if to handle the first case - removing a leaf node, aka
+        * a node that doesn't have children.
+        * Simply set that node to null, and return that node (which will be null now) to its
+        * parent so that we can assign the pointer. The parent will now point to null because
+        * that node has been removed.
+        **/
         if (!node.left && !node.right){
             node = null;
             return node;
         }else if (!node.left && node.right){
+            /**
+            * This statement and the else if statement below handles the second case:
+            * aka, when a node has only one child.
+            * For example, if the left node is null and the right node exists, we set 
+            * node = node.right
+            * Then we return the node to its new parent. 
+            * Use the tree we created in this file for instance:
+            * If we are removing the node with the value of 5, we will call it node_5.
+            * node_5 has a parent node_7 and a left child node_3.
+            * To remove node_5, we set node_5 = node_5.left, which means node_5 = node_3
+            * node_5 is now gone, we return node_3 to node_7.
+            * You see, we removed a node and basically ask its parent to point to the grandchild. 
+            **/
             node = node.right;
             return node;
         }else if (node.left && !node.right){
             node = node.left;
             return node;
         }else {
+            /**
+            * This is the last case, which is when the node has two children.
+            * This one is a little tricky. We must remove the node in a way that doesn't affect 
+            * the balance of the tree after the removal. 
+            * Refer to the comment on the top.
+            **/
             let minNode = findMinNode(node.right);
             node.key = minNode.key;
             node.right = self.removeNode(node.right, minNode.key);
